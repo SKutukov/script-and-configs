de5ee397a7c6ac7821cba4e5c608ba5d0d6b93d1
Sergey Kutukov <skutukov96@gmail.com>
2017-09-07 08:52:00 +0300

    Update fix_gui.sh

--- fix_gui.sh
+++ fix_gui.sh
@@ -1,5 +1,25 @@
-#fix gui in ubuntu
-sudo apt-get update
-sudo apt-get upgrade
-sudo apt-get --reinstall install unity-setting-daemon
-sudo reboot
+if [ -d "$3" ]
+  then 
+	echo "directory $3 already exist"
+  else
+	mkdir $3
+fi
+for file in $1/*
+do 
+  
+  parts=$(echo $file | tr "/" "\n")
+
+	for addr in $parts
+	do
+    	name=$addr
+	done
+  if [ -a "$3/$name" ]
+  then 
+	echo "$file already exist"
+  else  
+	echo "processing $file"
+  	#ffmpeg -i $file -vf "transpose=$2" $3/$name	
+	ffmpeg -i $file -c copy -metadata:s:v:0 rotate=90 $3/$name
+
+  fi
+done
