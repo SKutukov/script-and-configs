702ee43c4d3d034c97b221a7d2f6ab9b881f42f4
SKutukov <skutukov96@gmail.com>
2019-01-17 20:31:56 +0300

    i3 config for asus K55VD

---
+++ i3/asus K55VD/install.sh
@@ -0,0 +1,16 @@
+sudo apt-get install i3lock-fancy
+sudo apt-get install feh
+sudo apt-get install thunar
+sudo apt-get install lxappearance
+sudo apt-get install arc-theme
+
+sudo add-apt-repository -u ppa:snwh/ppa
+sudo apt-get install moka-icon-theme faba-icon-theme faba-mono-icons
+
+
+sudo apt-get install rofi
+sudo apt-get install compton
+
+sudo apt-get install i3blocks
+
+
