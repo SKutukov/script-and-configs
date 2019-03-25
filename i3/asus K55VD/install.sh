50c9af050b146c0348ba9dda1260ad00470f144c
Sergey Kutukov <skutukov96@gmail.com>
2019-03-25 15:11:20 +0300

    Update install.sh

--- i3/asus K55VD/install.sh
+++ i3/asus K55VD/install.sh
@@ -1,16 +1,16 @@
-sudo apt-get install i3lock-fancy
-sudo apt-get install feh
-sudo apt-get install thunar
-sudo apt-get install lxappearance
-sudo apt-get install arc-theme
+sudo apt-get install i3lock-fancy -y
+sudo apt-get install feh -y
+sudo apt-get install thunar -y 
+sudo apt-get install lxappearance -y 
+sudo apt-get install arc-theme -y
 
 sudo add-apt-repository -u ppa:snwh/ppa
-sudo apt-get install moka-icon-theme faba-icon-theme faba-mono-icons
+sudo apt-get install moka-icon-theme faba-icon-theme faba-mono-icons -y
 
 
-sudo apt-get install rofi
-sudo apt-get install compton
+sudo apt-get install rofi -y
+sudo apt-get install compton -y
 
-sudo apt-get install i3blocks
+sudo apt-get install i3blocks -y
 
 
