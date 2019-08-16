8224c0272892d0f0e0c54724280bec997f7f45dc
Sergey Kutukov <skutukov96@gmail.com>
2019-08-16 22:11:12 +0300

    add i3 config for t480

---
+++ i3/t480/install.sh
@@ -0,0 +1,16 @@
+sudo apt-get install i3lock-fancy -y
+sudo apt-get install feh -y
+sudo apt-get install thunar -y 
+sudo apt-get install lxappearance -y 
+sudo apt-get install arc-theme -y
+
+sudo add-apt-repository -u ppa:snwh/ppa
+sudo apt-get install moka-icon-theme faba-icon-theme faba-mono-icons -y
+
+
+sudo apt-get install rofi -y
+sudo apt-get install compton -y
+
+
+sudo pip3 install i3pystatus colour netifaces 
+
